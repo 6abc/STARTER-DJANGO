6cfd1fea458867927b6ab73b5be07be7dc262a13
Ashish Thakur <97246854+6abc@users.noreply.github.com>
2025-11-07 01:33:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations app1
+python manage.py migrate app1
+python manage.py makemigrations accounts
+python manage.py migrate accounts
+python manage.py makemigrations
 python manage.py migrate
